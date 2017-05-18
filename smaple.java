a1962dc239458b9c7da6a0b2abedc0f5c6d86610
awsd3899 <awsd3899@gmail.com>
2017-05-18 10:32:53 +0530

    Update smaple.java

--- smaple.java
+++ smaple.java
@@ -1,13 +1,7 @@
-class FreshJuice {
-   enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
-   FreshJuiceSize size;
-}
 
-public class FreshJuiceTest {
-
-   public static void main(String args[]) {
-      FreshJuice juice = new FreshJuice();
-      juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
-      System.out.println("Size: " + juice.size);
+public class smaple {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
    }
 }
